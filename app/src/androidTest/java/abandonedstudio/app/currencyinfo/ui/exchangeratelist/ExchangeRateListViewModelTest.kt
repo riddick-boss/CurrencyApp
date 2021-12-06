0a0e1844f2135898f628ccd8a2d2333ad86973f9
Pawel Kremienowski <kremienowski33@gmail.com>
2021-12-06 14:29:48 +0100

    setup for ViewModel test

---
+++ app/src/androidTest/java/abandonedstudio/app/currencyinfo/ui/exchangeratelist/ExchangeRateListViewModelTest.kt
@@ -0,0 +1,20 @@
+package abandonedstudio.app.currencyinfo.ui.exchangeratelist
+
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+@HiltAndroidTest
+class ExchangeRateListViewModelTest {
+
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
